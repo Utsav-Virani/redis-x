faf3944654fc099be1c10953a7e3b0d588c59708
Utsav-Virani <uvvirani004@gmail.com>
2024-06-11 15:18:25 -0400

    Completed implimentation of hset, hget, set, and get

--- handler.go
+++ handler.go
@@ -6,8 +6,21 @@
 
 package main
 
+import "sync"
+
+var SETs = map[string]string{}
+var SETsmu = sync.RWMutex{} // because server is concurrent
+
+var HSETs = map[string]map[string]string{}
+var HSETsmu = sync.RWMutex{}
+
 var Handlers = map[string]func([]Value) Value{
-	"PING": ping,
+	"PING":    ping,
+	"SET":     set,
+	"GET":     get,
+	"HSET":    hset,
+	"HGET":    hget,
+	"HGETALL": hgetall,
 }
 
 func ping(args []Value) Value {
@@ -15,4 +28,92 @@ func ping(args []Value) Value {
 		return Value{typ: "string", str: "PONG"}
 	}
 	return Value{typ: "string", str: args[0].bulk}
+}
+
+func set(args []Value) Value {
+	if len(args) != 2 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'set' command"}
+	}
+	key := args[0].bulk
+	value := args[1].bulk
+
+	SETsmu.Lock()
+	SETs[key] = value
+	SETsmu.Unlock()
+
+	return Value{typ: "string", str: "OK"}
+}
+
+func get(args []Value) Value {
+	if len(args) != 1 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'get' command"}
+	}
+	key := args[0].bulk
+
+	SETsmu.RLock()
+	value, ok := SETs[key]
+	SETsmu.RUnlock()
+
+	if !ok {
+		return Value{typ: "null"}
+	}
+	return Value{typ: "bulk", bulk: value}
+}
+
+func hset(args []Value) Value {
+	if len(args) != 3 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'hset' command"}
+	}
+	key := args[0].bulk
+	field := args[1].bulk
+	value := args[2].bulk
+
+	HSETsmu.Lock()
+	if _, ok := HSETs[key]; !ok {
+		HSETs[key] = map[string]string{}
+	}
+	HSETs[key][field] = value
+	HSETsmu.Unlock()
+
+	return Value{typ: "string", str: "OK"}
+}
+
+func hget(args []Value) Value {
+	if len(args) != 2 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'hget' command"}
+	}
+	key := args[0].bulk
+	field := args[1].bulk
+
+	SETsmu.RLock()
+	value, ok := HSETs[key][field]
+	SETsmu.RUnlock()
+
+	if !ok {
+		return Value{typ: "null"}
+	}
+	return Value{typ: "bulk", bulk: value}
+}
+
+func hgetall(args []Value) Value {
+	if len(args) != 1 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'hgetall' command"}
+	}
+	key := args[0].bulk
+
+	SETsmu.RLock()
+	fields, ok := HSETs[key]
+	SETsmu.RUnlock()
+
+	if !ok {
+		return Value{typ: "null"}
+	}
+
+	var array []Value
+	for field, value := range fields {
+		array = append(array, Value{typ: "bulk", bulk: field})
+		array = append(array, Value{typ: "bulk", bulk: value})
+	}
+
+	return Value{typ: "array", array: array}
 }
